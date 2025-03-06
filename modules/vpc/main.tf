92b03c7ddbd5607605e80fb75f6e2bbf2e19dc2d
Maciej Łopalewski <maciej.lopalewski@uninterrupted.tech>
2025-03-06 14:31:16 +0100

    fix: Enable DNS hostnames in VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,7 +13,9 @@ locals {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
 
   tags = merge(local.tags, { Name = local.prefix })
 }
