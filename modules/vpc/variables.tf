b9f7f299e36cebfff85c2cb3a32921547ac993ee
Maciej Łopalewski <112621777+u11d-maciej-lopalewski@users.noreply.github.com>
2025-03-06 14:42:33 +0100

    fix: Enable DNS hostnames in VPC (#9)

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -17,3 +17,15 @@ variable "az_count" {
   description = "Number of AZs to cover in a given region."
   type        = number
 }
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
