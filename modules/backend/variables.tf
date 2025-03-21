69a08142a80ec83bce37e0d940fbcf45ad932787
u11d-pawel-swiridow <109523122+u11d-pawel-swiridow@users.noreply.github.com>
2025-03-21 12:34:41 +0100

    Merge fix (#19)

--- modules/backend/variables.tf
+++ modules/backend/variables.tf
@@ -191,3 +191,29 @@ variable "acm_certificate_arn" {
     error_message = "The ACM certificate must be in the us-east-1 region for use with CloudFront."
   }
 }
+
+variable "deployment_circuit_breaker" {
+  description = "Deployment circuit breaker configuration"
+  type = object({
+    enable   = bool
+    rollback = bool
+  })
+  default = null
+}
+
+variable "health_check_grace_period_seconds" {
+  description = "The grace period for health checks in seconds"
+  type        = number
+  default     = null
+}
+
+variable "ecs_container_insights" {
+  description = "Enable container insights for the ECS cluster"
+  type        = string
+  default     = "disabled"
+
+  validation {
+    condition     = contains(["enabled", "enhanced", "disabled"], var.ecs_container_insights)
+    error_message = "The ecs_container_insights value must be either 'enabled', 'enhanced' or 'disabled'."
+  }
+}
